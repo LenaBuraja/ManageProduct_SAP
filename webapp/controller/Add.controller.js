c54cd49e501de4e73674ff7400edcbc3d2d1d0da
LenaBuraja <lenusik343buraya@gmail.com>
2019-09-13 12:25:57 +0000

    Week 4. Unit 2/3

--- webapp/controller/Add.controller.js
+++ webapp/controller/Add.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define(
 	[
 		"./BaseController",
-		"sap/ui/core/routing/History"
+		"sap/ui/core/routing/History",
+		"sap/m/MessageToast"
 	],
-	function(BaseController, History) {
+	function(BaseController, History, MessageToast) {
 		"use strict";
 		
 		return BaseController.extend("opensap.managepropducts.ManageProducts.controller.Add", {
@@ -48,12 +49,45 @@ sap.ui.define(
 					
 				// create new entry in the model
 				this._oContext = this.getModel().createEntry("/ProductSet", {
-					properties: oProperties
+					properties: oProperties,
+					success: this._onCreateSuccess.bind(this)
 				});
 				
 				// bind the view to the new entry
 				this.getView().setBindingContext(this._oContext);
+			},
+			
+			_onCreateSuccess: function (oProduct) {
 				
+				// navigate to the new product's object view
+				var sId = oProduct.ProductID;
+				this.getRouter().navTo("object", {
+					objectId : sId
+				}, true);
+				// unbind the view to not show this object again
+				this.getView().unbindObject();
+				// show success messge
+				var sMessage = this.getResourceBundle()
+					.getText("newObjectCreated", [oProduct.Name]);
+				MessageToast.show(sMessage, {
+					closeOnBrowserNavigation : false
+				});
+			},
+			
+			/**
+			 * Event handler for the cancel action
+			 * @public
+			 */
+			onCancel: function() {
+				this.onNavBack();
+			},
+			
+			/**
+			 * Event handler for the save action
+			 * @public
+			 */
+			onSave: function() {
+				this.getModel().submitChanges();
 			},
 			
 			/**
@@ -62,8 +96,10 @@ sap.ui.define(
 			 * If not, it will replace the current entry of the browser history with the worklist route.
 			 * @public
 			 */
-			 
-			onNavBack : function() {
+			
+			onNavBack: function() {
+				// discard new product from model.
+				this.getModel().deleteCreatedEntry(this._oContext);
 				var oHistory = History.getInstance(),
 				sPreviousHash = oHistory.getPreviousHash();
 				
